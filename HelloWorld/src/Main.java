815d498328d199e1b3cb1c10bb7bb59febedab6d
Minh Quý <120252010+nmquys@users.noreply.github.com>
2024-10-10 01:11:44 +0700

    Add files via upload

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int x = 1;
+        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter name: ");
+        String name = sc.nextLine();
+        System.out.println("Xin chao: " + name);
+    }
+}
